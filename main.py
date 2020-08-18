01b95501670fe0fdaf1e4327efe725a91d96da4a
Nahuel <juradonahuel20@gmail.com>
2020-08-18 16:39:47 -0300

    Agregamos el primer main

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Hola")
+
+a = int(input("Ingrese un numero"))
+b = int(input("Ingrese otro numero"))
+
+print("suma:",a+b)
+a = int(input("Ingrese numero:"))
+b = int(input("Ingrese numero:"))
+print("Suma:",a+b)
+print("resta:",a+b)
+print("multi:",a+b)
+print("div:",a+b)
